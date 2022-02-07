55ad307e61ff212477712d78f2bd5541cfc7c87b
Jessica Stokes <hello@jessicastokes.net>
2022-02-07 13:31:22 -0800

    Move into a module for niceness

--- src/lib.rs
+++ src/lib.rs
@@ -1,97 +1,167 @@
-use serde_derive::Deserialize;
-
-#[derive(Deserialize)]
-struct Config {
-    server: ServerConfig,
-    feeds: std::collections::HashMap<String, FeedConfig>,
-    downloader: DownloaderConfig,
+/// Represents a given RSS channel, which points at a video feed.
+pub struct Channel {
+    name: String,
+    target_url: String,
+    limit: u16,
+    pub rss_channel: Option<rss::Channel>,
 }
 
-#[derive(Deserialize)]
-struct DownloaderConfig {
-    self_update: bool,
-    timeout: Option<u32>
-}
+// TODO:
+// - Read channel from disk if already extant
+// - Update method which looks at existing episodes to adjust
+// - Write channel to disk
+// - Download specific episodes
+impl Channel {
+    pub fn new_with_limit(name: String, target_url: String, limit: u16) -> Self {
+        let rss_channel = match std::fs::File::open(format!("{}.rss", name)) {
+            Ok(file) => {
+                let reader = std::io::BufReader::new(file);
+                match rss::Channel::read_from(reader) {
+                    Ok(channel) => Some(channel),
+                    Err(_) => None,
+                }
+            },
+            Err(_) => None
+        };
 
-fn default_page_size() -> u16 { 50 }
-fn default_update_period() -> String { "6h".to_string() }
+        Self {
+            name,
+            target_url,
+            limit,
+            rss_channel,
+        }
+    }
 
-#[derive(Deserialize)]
-struct FeedConfig {
-    url: String,
-    #[serde(default = "default_page_size")]
-    page_size: u16,
-    #[serde(default = "default_update_period")]
-    update_period: String,
-    youtube_dl_args: Vec<String>,
-}
+    pub fn new(name: String, target_url: String) -> Self {
+        Self::new_with_limit(name, target_url, 50)
+    }
 
-#[derive(Deserialize)]
-struct ServerConfig {
-    hostname: Option<String>,
-    bind_address: Option<String>,
-    port: u32,
-    data_dir: String,
+    pub fn update(&mut self) {
+        let ytdl_result = youtube_dl::YoutubeDl::new(self.target_url.clone())
+            .youtube_dl_path("yt-dlp")
+            // .extra_arg("--flat-playlist") // NOTE: This makes very long playlists work, but misses out on lots of metadata
+            .extra_arg("--playlist-end")
+            .extra_arg(self.limit.to_string())
+            .extra_arg("--format")
+            .extra_arg("bestvideo[ext=mp4][vcodec^=avc1]+bestaudio[ext=m4a]/best[ext=mp4][vcodec^=avc1]/best[ext=mp4]/best")
+            // .extra_arg("--output")
+            // .extra_arg()
+            .run()
+            .unwrap();
+
+        log::debug!("{:#?}", ytdl_result);
+
+        if let youtube_dl::YoutubeDlOutput::Playlist(playlist) = ytdl_result {
+            let title = playlist.title.as_ref().unwrap_or_else(|| { &self.target_url }).clone();
+
+            let rss_items: Vec<rss::Item> = match playlist.entries {
+                Some(ref entries) => entries
+                    .into_iter()
+                    .map(|video| {
+                        use hhmmss::Hhmmss;
+
+                        let duration = match &video.duration {
+                            Some(value) => {
+                                let secs = match value {
+                                    serde_json::Value::Number(secs) => secs.as_f64().unwrap_or(0.0),
+                                    _ => 0.0,
+                                };
+                                std::time::Duration::new(secs as u64, 0)
+                            }
+                            None => std::time::Duration::default(),
+                        };
+
+                        let upload_date = video.upload_date.as_ref().map(|date| {
+                            chrono::Date::<chrono::Utc>::from_utc(
+                                chrono::NaiveDate::parse_from_str(&date, "%Y%m%d").unwrap(),
+                                chrono::Utc,
+                            )
+                            .and_hms(0, 0, 0)
+                            .to_rfc2822()
+                        });
+
+                        let item_itunes_extension =
+                            rss::extension::itunes::ITunesItemExtensionBuilder::default()
+                                .author(title.clone())
+                                .subtitle(video.title.clone())
+                                .summary(video.description.clone())
+                                .image(video.thumbnail.clone())
+                                .duration(duration.hhmmss())
+                                .explicit("No".to_string())
+                                .build();
+
+                        let item_enclosure = rss::EnclosureBuilder::default()
+                            .url(format!("http://192.168.1.193:8000/{}.mp4", video.id)) // TODO: This has to be absolute!
+                            .length((video.filesize_approx.unwrap_or(0.0) as u64).to_string())
+                            .mime_type("video/mp4")
+                            .build();
+
+                        rss::ItemBuilder::default()
+                            .guid(rss::GuidBuilder::default().value(video.id.clone()).build())
+                            .title(video.title.clone())
+                            .link(video.webpage_url.clone())
+                            .pub_date(upload_date)
+                            .enclosure(item_enclosure)
+                            .itunes_ext(item_itunes_extension)
+                            .build()
+                    })
+                    .collect(),
+                None => vec![],
+            };
+
+            let mut rss_channel = self.rss_channel.clone().unwrap_or_else(|| {
+                let link = match playlist.webpage_url {
+                    Some(url) => url,
+                    None => self.target_url.to_string(),
+                };
+
+                let description = format!("Vodsync podcast feed for {}", title);
+
+                let rss_itunes_category = rss::extension::itunes::ITunesCategoryBuilder::default()
+                    .text("TV & Film")
+                    .build();
+
+                let rss_itunes_extension =
+                    rss::extension::itunes::ITunesChannelExtensionBuilder::default()
+                        .author(title.clone())
+                        .subtitle(title.clone())
+                        .summary(description.clone())
+                        .explicit("No".to_string())
+                        // TODO: .image
+                        .category(rss_itunes_category)
+                        .block("Yes".to_string())
+                        .build();
+
+
+                rss::ChannelBuilder::default()
+                    .title(title)
+                    .link(link)
+                    .description(description)
+                    .generator("Vodsync (https://github.com/ticky/vodsync)".to_string())
+                    .itunes_ext(rss_itunes_extension)
+                    .build()
+            });
+
+            rss_channel.set_items(rss_items);
+
+            self.rss_channel = Some(rss_channel);
+        } else {
+            panic!("This URL points to a single video, not a channel!")
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
     #[test]
-    fn test_config_parser() {
-        // This configuration is for PodSync, but we aim to be compatible
-        let config: super::Config = toml::from_str(r#"
-            [server]
-            hostname = "http://connie.local:8080"
-            port = 8080
-            # Don't change if you run podsync via docker
-            data_dir = "/app/data"
-
-            [tokens]
-            # YouTube API Key. See https://developers.google.com/youtube/registering_an_application
-            youtube = "aaaaaaaaa"
-
-            [feeds]
-              [feeds.chipcheezumlps]
-              url = "https://www.youtube.com/user/ChipCheezumLPs"
-              page_size = 10
-              update_period = "6h"
-              quality = "high"
-              format = "video"
-              clean = { keep_last = 30 }
-              filters = { not_title = "Cut Commentary" }
-              opml = true
-              youtube_dl_args = [ "--write-sub", "--write-auto-sub", "--embed-subs", "--sub-lang", "en" ]
-
-            [downloader]
-            self_update = true # Optional, auto update youtube-dl every 24 hours
-
-            [database]
-            badger = { truncate = true, file_io = true }
-        "#).unwrap();
-
-        assert_eq!(config.server.hostname, Some("http://connie.local:8080".to_string()));
-        assert_eq!(config.server.bind_address, None);
-        assert_eq!(config.server.port, 8080);
-        assert_eq!(config.server.data_dir, "/app/data");
-
-        assert_eq!(
-            config.feeds["chipcheezumlps"].url,
-            "https://www.youtube.com/user/ChipCheezumLPs"
+    fn test_twitch_rss_valid() {
+        use rss::validation::Validate;
+        let mut channel = super::Channel::new_with_limit(
+            "zandravandra".to_string(),
+            "https://www.twitch.tv/zandravandra/videos".to_string(),
+            2,
         );
-        assert_eq!(config.feeds["chipcheezumlps"].page_size, 10);
-        assert_eq!(config.feeds["chipcheezumlps"].update_period, "6h");
-        assert_eq!(
-            config.feeds["chipcheezumlps"].youtube_dl_args,
-            [
-                "--write-sub",
-                "--write-auto-sub",
-                "--embed-subs",
-                "--sub-lang",
-                "en"
-            ]
-        );
-
-        assert_eq!(config.downloader.self_update, true);
-        assert_eq!(config.downloader.timeout, None);
+        channel.update();
+        channel.rss_channel.unwrap().validate().unwrap();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,131 +1,14 @@
-fn rss_channel_from(target_url: String, limit: u64) -> rss::Channel {
-    let channel = youtube_dl::YoutubeDl::new(target_url.clone())
-        .youtube_dl_path("yt-dlp")
-        // .extra_arg("--flat-playlist") // NOTE: This makes very long playlists work, but misses out on lots of metadata
-        .extra_arg("--playlist-end")
-        .extra_arg(limit.to_string())
-        .run()
-        .unwrap();
-
-    log::debug!("{:#?}", channel);
-
-    if let youtube_dl::YoutubeDlOutput::Playlist(playlist) = channel {
-        let title = match playlist.title {
-            Some(title) => title,
-            None => target_url.to_string(),
-        };
-
-        let link = match playlist.webpage_url {
-            Some(url) => url,
-            None => target_url,
-        };
-
-        let description = format!("Vodsync podcast feed for {}", title);
-
-        let rss_itunes_category = rss::extension::itunes::ITunesCategoryBuilder::default()
-            .text("TV & Film")
-            .build();
-
-        let rss_itunes_extension = rss::extension::itunes::ITunesChannelExtensionBuilder::default()
-            .author(title.clone())
-            .subtitle(title.clone())
-            .summary(description.clone())
-            .explicit("No".to_string())
-            // TODO: .image
-            .category(rss_itunes_category)
-            .block("Yes".to_string())
-            .build();
-
-        let rss_items: Vec<rss::Item> = match playlist.entries {
-            Some(entries) => entries
-                .into_iter()
-                .map(|video| {
-                    use hhmmss::Hhmmss;
-
-                    let duration = match video.duration {
-                        Some(value) => {
-                            let secs = match value {
-                                serde_json::Value::Number(secs) => secs.as_f64().unwrap_or(0.0),
-                                _ => 0.0,
-                            };
-                            std::time::Duration::new(secs as u64, 0)
-                        }
-                        None => std::time::Duration::default(),
-                    };
-
-                    let upload_date = video.upload_date.map(|date| {
-                        chrono::Date::<chrono::Utc>::from_utc(
-                            chrono::NaiveDate::parse_from_str(&date, "%Y%m%d").unwrap(),
-                            chrono::Utc,
-                        )
-                        .and_hms(0, 0, 0)
-                        .to_rfc2822()
-                    });
-
-                    let item_itunes_extension =
-                        rss::extension::itunes::ITunesItemExtensionBuilder::default()
-                            .author(title.clone())
-                            .subtitle(video.title.clone())
-                            .summary(video.description)
-                            .image(video.thumbnail)
-                            .duration(duration.hhmmss())
-                            .explicit("No".to_string())
-                            .build();
-
-                    let item_enclosure = rss::EnclosureBuilder::default()
-                        .url(format!("http://192.168.1.193:8000/{}.mp4", video.id)) // TODO: This has to be absolute!
-                        .length((video.filesize_approx.unwrap_or(0.0) as u64).to_string())
-                        .mime_type("video/mp4")
-                        .build();
-
-                    rss::ItemBuilder::default()
-                        .guid(rss::GuidBuilder::default().value(video.id).build())
-                        .title(video.title)
-                        .link(video.webpage_url)
-                        .pub_date(upload_date)
-                        .enclosure(item_enclosure)
-                        .itunes_ext(item_itunes_extension)
-                        .build()
-                })
-                .collect(),
-            None => vec![],
-        };
-
-        rss::ChannelBuilder::default()
-            .title(title)
-            .link(link)
-            .description(description)
-            .generator("Vodsync (https://github.com/ticky/vodsync)".to_string())
-            .itunes_ext(rss_itunes_extension)
-            .items(rss_items)
-            .build()
-    } else {
-        panic!("This URL points to a single video, not a channel!")
-    }
-}
-
 fn main() {
     env_logger::init();
 
     // let target_url = "https://www.twitch.tv/zandravandra/videos";
     let target_url = "https://www.youtube.com/c/mightycarmods";
+    // let target_url = "QWkUFkXcx9I";
 
-    print!(
-        "{}",
-        rss_channel_from(target_url.to_string(), 10).to_string()
-    );
-}
+    // TODO: Web server!
 
-#[test]
-fn test_youtube_rss_valid() {
-    use rss::validation::Validate;
-    let channel = rss_channel_from("https://www.youtube.com/c/mightycarmods".to_string(), 2);
-    channel.validate().unwrap();
-}
+    let mut channel = vodsync::Channel::new_with_limit("test".to_string(), target_url.to_string(), 1);
+    channel.update();
 
-#[test]
-fn test_twitch_rss_valid() {
-    use rss::validation::Validate;
-    let channel = rss_channel_from("https://www.twitch.tv/zandravandra/videos".to_string(), 2);
-    channel.validate().unwrap();
+    print!("{}", channel.rss_channel.unwrap().to_string());
 }
