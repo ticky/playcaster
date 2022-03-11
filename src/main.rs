e74e3cac53aa8f0a71d65fb3f214854a12143e8d
Jessica Stokes <hello@jessicastokes.net>
2022-03-10 21:42:39 -0800

    Move to Clap 3.x instead of Structopt

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@ extern crate log;
 use anyhow::Result;
 use std::fs::OpenOptions;
 use std::path::PathBuf;
-use structopt::StructOpt;
+use clap::Parser;
 use url::Url;
 
 use playcaster::Channel;
 
-#[derive(StructOpt, Debug)]
+#[derive(Parser, Debug)]
 /// Turn any playlist into a Podcast feed
 struct Args {
     /// Path to the channel's RSS feed file
-    #[structopt(parse(from_os_str))]
+    #[clap(parse(from_os_str))]
     feed_file: PathBuf,
 
     /// Base URL to server which will serve the feed items
@@ -20,25 +20,25 @@ struct Args {
 
     /// Playlist URL to download videos from.
     /// Required if creating a new feed, or if the feed's link element doesn't already point to a playlist URL.
-    #[structopt(long)]
+    #[clap(long)]
     playlist_url: Option<Url>,
 
     /// Maximum number of videos to download for the given channel
-    #[structopt(default_value = "30", long)]
+    #[clap(default_value = "30", long)]
     limit: usize,
 
     /// Maximum number of videos to keep for the given channel.
     /// Any older videos will be deleted when the feed updates.
     /// Should be greater than or equal to `limit`.
-    #[structopt(long)]
+    #[clap(long)]
     keep: Option<usize>,
 
     /// Do not write the updated RSS feed to disk; just print it to the terminal
-    #[structopt(long)]
+    #[clap(long)]
     no_write_feed: bool,
 
     /// Write terse RSS XML to disk, rather than the default pretty-printed version
-    #[structopt(long)]
+    #[clap(long)]
     no_pretty: bool,
 
     /// Additional arguments to be passed to `yt-dlp`
@@ -48,7 +48,7 @@ struct Args {
 fn main() -> Result<()> {
     env_logger::init();
 
-    let args = Args::from_args();
+    let args = Args::parse();
 
     println!("Starting up...");
 
